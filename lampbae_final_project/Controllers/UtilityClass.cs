2016c123a23e36a7ed086ef1dd28378c3b8e8485
saunlani <saunlani@gmail.com>
2018-06-27 14:34:55 -0400

    fixed user ip pull

--- lampbae_final_project/Controllers/UtilityClass.cs
+++ lampbae_final_project/Controllers/UtilityClass.cs
@@ -15,7 +15,8 @@ namespace lampbae_final_project.Controllers
         // gets IP address of user
         public static string GetIP()
         {
-            string userIP = new WebClient().DownloadString("http://icanhazip.com");
+            //string userIP1 = new WebClient().DownloadString("http://icanhazip.com");
+            string userIP = HttpContext.Current.Request.UserHostAddress;
             return userIP;
         }
 
